905f3f8d16301a939b3b320e991f08d723d88666
dineshkumar080900 <157379162+dineshkumar080900@users.noreply.github.com>
2025-01-29 11:31:53 +0530

    Day1

---
+++ Node/pathandfs.js
@@ -0,0 +1,14 @@
+// var http = require('http');
+
+// const server=http.createServer((res,req)=>{
+// req.statusCode=200;
+// // req.write()
+// req.end("hi44444444")
+// })
+
+// server.listen(8080);
+
+
+
+const path=require('path')
+const c=path.join('--dirname','Path_and_fs.html','logo.png')
